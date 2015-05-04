1e4906ffba13aa69bfb7e769484fa47fee19dbd5
Patrick Losco <pjlosco@gmail.com>
2015-05-03 20:53:22 -0700

    Contain substring exercise

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,6 +1,3 @@
-/**
- * Created by patrick on 4/28/14.
- */
 public class Fibonacci {
 
     /**
@@ -11,7 +8,7 @@ public class Fibonacci {
     public static void main(String[] args) {
         System.out.println("Given a number and get the number in the fibonacci sequence");
         for (int i = 0; i<10; i++) {
-            System.out.println("input" +i+ " results in:");
+            System.out.println("input " +i+ " results in:");
             System.out.println(fibonacci(i));
         }
     }
