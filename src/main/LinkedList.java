ac4049d1225a22f379d4051abef9a1842ebf889f
Patrick Losco <pjlosco@gmail.com>
2015-08-05 18:25:53 -0700

    Updated a bunch of questions

--- src/main/LinkedList.java
+++ src/main/LinkedList.java
@@ -4,25 +4,84 @@ package main;
  * Created by patricklosco on 2/3/15.
  */
 public class LinkedList {
-    int value;
-    LinkedList restOfList;
 
-    public LinkedList reverseLinkedList(LinkedList list) {
-        LinkedList reversedList = new LinkedList();
-        if (list.restOfList == null) {
+    private int value;
+    private LinkedList restOfList;
 
+    public LinkedList(int value, LinkedList restOfList) {
+        this.value = value;
+        this.restOfList = restOfList;
+    }
+
+    public int getNodeValue() {
+        return value;
+    }
+
+    public LinkedList getRestOfList() {
+        return restOfList;
+    }
+
+    public LinkedList reverseList() {
+        LinkedList backwardsList = new LinkedList(value, null);
+        if (restOfList != null) {
+            restOfList.reverseList();
+            restOfList.addNewLink(backwardsList);
         } else {
-            reverseLinkedList(restOfList);
+
         }
-        return null;
+        return restOfList;
+
+        LinkedList reversedPart = null;
+        LinkedList current = head;
+        while (restOfList != null) {
+            LinkedList next = current.restOfList;
+            restOfList.restOfList = reversedPart;
+            reversedPart = current;
+            current = next;
+        }
+        head = reversedPart;
     }
 
-    public void addTop(LinkedList list) {
-
+    public void addNewNode(int newValue) {
+        restOfList = new LinkedList(value,restOfList);
+        this.value = newValue;
     }
 
-    public static void main(String[] args) {
+    public void addNewLink(LinkedList list) {
+        if (restOfList != null) {
+            restOfList.addNewLink(list);
+        } else {
+            restOfList = list;
+        }
+    }
 
+    public int removeNode(){
+        int returnValue = value;
+        value = restOfList.getNodeValue();
+        restOfList = restOfList.getRestOfList();
+        return returnValue;
+    }
+
+    public int removeEndLink(){
+        if (restOfList != null) {
+            if (restOfList.restOfList != null) {
+                return restOfList.removeEndLink();
+            } else {
+                int returnValue = restOfList.value;
+                restOfList = null;
+                return returnValue;
+            }
+        } else {
+            return value;
+        }
+    }
+
+    public String toString() {
+        if (restOfList != null) {
+            return value + " " + restOfList.toString();
+        } else {
+            return value + "";
+        }
     }
 
 }
---
+++ src/test/LinekdListTest.java
@@ -0,0 +1,46 @@
+package test;
+
+import junit.framework.Assert;
+import main.LinkedList;
+import org.junit.Test;
+
+/**
+ * Created by Patrick on 6/26/2015.
+ */
+public class LinekdListTest {
+
+    @Test
+    public void PopulateLinkedList() {
+        LinkedList myList = new LinkedList(10, null);
+        myList.addNewLink(new LinkedList(9, null));
+        myList.addNewNode(11);
+        Assert.assertEquals("11 10 9", myList);
+    }
+
+    @Test
+    public void ReverseLinkedList() {
+        LinkedList myList = new LinkedList(10, null);
+        myList.addNewLink(new LinkedList(9, null));
+        myList.addNewNode(11);
+        myList.reverseList();
+        Assert.assertEquals("9 10 11", myList);
+    }
+
+    @Test
+    public void RemoveNode() {
+        LinkedList myList = new LinkedList(10, null);
+        myList.addNewLink(new LinkedList(9, null));
+        myList.addNewNode(11);
+        org.junit.Assert.assertEquals(11, myList.removeNode());
+        Assert.assertEquals("10 9", myList.toString().trim());
+    }
+
+    @Test
+    public void RemoveLink() {
+        LinkedList myList = new LinkedList(10, null);
+        myList.addNewLink(new LinkedList(9, null));
+        myList.addNewNode(11);
+        Assert.assertEquals(9, myList.removeEndLink());
+        Assert.assertEquals("11 10", myList.toString().trim());
+    }
+}
